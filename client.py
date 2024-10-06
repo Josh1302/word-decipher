b5946305d6167bae03f381a89fe14f9754919b36
Joshua Underwood <119467345+Josh1302@users.noreply.github.com>
2024-10-06 18:45:42 -0500

    Update client.py

--- client.py
+++ client.py
@@ -1,26 +1,14 @@
 #!/usr/bin/env python3
-
-
 import sys
 import socket
 import selectors
 import types
+import traceback
 
 sel = selectors.DefaultSelector()
 
-def create_message():
-    message = ""
-    if len(sys.argv) < 2:
-        print("usage:", sys.argv[0], "<message>")
-        sys.exit(1)
-    message = sys.argv[1]
-    return message
 
-messages = create_message()
-
-# this routine is called to create each of the many ECHO CLIENTs we want to create
-
-def start_connections(host, port, num_conns):
+def start_connections(host, port, num_conns, messages):
     server_addr = (host, port)
     for i in range(0, num_conns):
         connid = i + 1
@@ -33,55 +21,55 @@ def start_connections(host, port, num_conns):
             connid=connid,
             msg_total=sum(len(m) for m in messages),
             recv_total=0,
-            messages=messages,
+            messages=list(messages),
             outb=b"",
         )
         sel.register(sock, events, data=data)
 
-# this routine is called when a client triggers a read or write event
 
+# this routine is called when a client triggers a read or write event
 def service_connection(key, mask):
     sock = key.fileobj
     data = key.data
     if mask & selectors.EVENT_READ:
         recv_data = sock.recv(1024)  # Should be ready to read
         if recv_data:
-            print("received", repr(recv_data), "from connection 1")
-            data.recv_total = data.msg_total
+            print("received", repr(recv_data), "from connection", data.connid)
+            data.recv_total += len(recv_data)
         if not recv_data or data.recv_total == data.msg_total:
             print("closing connection", data.connid)
             sel.unregister(sock)
             sock.close()
     if mask & selectors.EVENT_WRITE:
         if not data.outb and data.messages:
-            data.outb = data.messages
+            data.outb = data.messages.pop(0).encode()  # Convert to bytes
         if data.outb:
-            print("sending", repr(data.outb), "to connection 1")
-            sent = sock.send(bytes(data.outb,'utf-8')) # Should be ready to write
+            print("sending", repr(data.outb), "to connection", data.connid)
+            sent = sock.send(data.outb)  # Should be ready to write
             data.outb = data.outb[sent:]
 
 
+if len(sys.argv) != 5:
+    print("usage:", sys.argv[0], "<host> <port> <message1> <message2>")
+    sys.exit(1)
 
-# main program
+host, port = sys.argv[1], int(sys.argv[2])
+messages = [sys.argv[3], sys.argv[4]]  # Ensure messages is a list of strings
+num_conns = 2  # You can change this to however many clients you want to create
 
-host = '0.0.0.0'   # localhost; use 0.0.0.0 if you want to communicate across machines in a real network
-port = 12345         # I just love fibonacci numbers
-num_conns = 2       # you can change this to however many clients you want to create
+# Start the connections
+start_connections(host, port, num_conns, messages)
 
-
-start_connections(host, port, num_conns)
-
-# the event loop
-
+# Event loop
 try:
     while True:
         events = sel.select(timeout=1)
-        if events:
-            for key, mask in events:
+        for key, mask in events:
+            if key.data:
                 service_connection(key, mask)
         # Check for a socket being monitored to continue.
-                sel.get_map()
-                break
+        if not sel.get_map():
+            break
 except KeyboardInterrupt:
     print("caught keyboard interrupt, exiting")
 finally:
