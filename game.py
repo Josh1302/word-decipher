678afb426f0a3dfda56562c5b32f216f45835286
Josh1302 <josh1302@colostate.edu>
2024-11-17 14:18:36 -0700

    full file

--- game.py
+++ game.py
@@ -1,16 +1,47 @@
+#!/usr/bin/env python3
+import random
 class Game:
-    def __init__(self,guesses,word_size):
+    def __init__(self,guesses,word_size,file):
         self.guesses = guesses
         self.word_length = word_size
-    
-    def dict(self,file):
+        self.dict = []
         try:
-            with open(file) as file:
-                s = []
-                for i in file:
-                    if(len(i) == self.word_length):
-                        s.append(i)
+            with open(file,"r") as file:
+                for line in file:
+                    for words in line.split():
+                        if(len(words) == self.word_length):
+                            self.dict.append(words)
         except FileNotFoundError:
             print("Error: File Not Found!"+file)
         except Exception as e:
             print("Error: ",e)
+        self.word = self.select_word()
+        
+
+    def select_word(self):
+        rand = random.randint(0,len(self.dict)-1)
+        self.word = self.dict[rand]
+        return self.word
+    
+    def contains(self,word):
+        for i in self.dict:
+            if(i == word):
+                return True
+        return False
+    
+    def hint(self,guess):
+        hint = ""
+        if len(guess) != len(self.word):
+            print("Error: Guess is not the correct length. Length is:",self.word_length)
+        for i in range(0,len(guess),1):
+            if guess[i] == self.word[i]:
+                hint += "="
+            else:
+                hint += '-'
+        return hint
+
+            
+    
+
+
+    
